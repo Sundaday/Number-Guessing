576c0c3b63bfb4bc9064499ee7db629f7fdaf575
Sundaday <john.koch.pro@gmail.com>
2022-03-08 12:50:21 +0100

    done

--- Number Guessing/Program.cs
+++ Number Guessing/Program.cs
@@ -6,7 +6,34 @@ namespace Number_Guessing
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Random r = new Random();
+
+            int winNum = r.Next(0,100);
+            bool win = false;
+
+            do 
+            {
+                Console.WriteLine("Guess a number in between 0 and 100");
+                string a = Console.ReadLine();
+
+                int e = int.Parse(a);
+
+                if( e > winNum)
+                {
+                    Console.WriteLine("To High");
+                }
+                else if( e < winNum)
+                {
+                    Console.WriteLine("Go low");
+                } else if ( e == winNum)
+                {
+                    Console.WriteLine("YOU WIN");
+                    win = true;
+                }
+            } 
+            while (win==false);
+
+            Console.WriteLine();
         }
     }
 }
